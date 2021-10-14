ffd8a0f1eefd1e264978864d4cbbc4de63485c57
Dorin Pomian <dorin.pomian@amadeus.com>
2021-10-14 21:46:45 +0200

    Improve configure script. Improve README

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+## Configure
+```
+python configure.py
+```
+#/!\ The master password is not saved anywhere, make sure you don't lose it!!! /!\
 ## Run unit tests
 ```
 python -m unittest discover ./tests
--- configure.py
+++ configure.py
@@ -10,11 +10,12 @@ from engine import pwd_utils
 
 
 def _run_generic_setup():
-    print(subprocess.run('./configure.sh'))
+    print(subprocess.run('source ./configure.sh'))
 
 
 def _generate_config_file():
     mpwd = getpass.getpass(prompt='Pick a master password: ')
+    print("\n/!\\ DON'T LOSE YOUR MASTER PASSWORD!! /!\\")
     config = utils.build_config(mpwd, pwd_utils.generate_salt())
     configfile = './res/prd/.config'
     with open(configfile, 'w') as ofile:
@@ -25,6 +26,7 @@ def _generate_config_file():
 def main():
     _run_generic_setup()
     _generate_config_file()
+    print('you can now run ./runweb.sh to start the webserver')
 
 
 if __name__ == '__main__':
